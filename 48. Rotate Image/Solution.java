78b3b8f5c552720d754d243c9d3385a15aec59e0
Jason <JasonHan0929@hotmail.com>
2017-02-23 00:09:31 -0500

    02/23/2017

---
+++ 48. Rotate Image/Solution.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1- i];
+            matrix[n - 1- i] = temp;
+        }
+        for (int i = 0; i < n; i ++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
+/*
+here give a common method to solve the image rotation problems.
+
+
+ //* clockwise rotate
+ //* first reverse up to down, then swap the symmetry 
+ //* 1 2 3     7 8 9     7 4 1
+ //* 4 5 6  => 4 5 6  => 8 5 2
+ //* 7 8 9     1 2 3     9 6 3
+
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
+
+
+//* anticlockwise rotate
+//* first reverse left to right, then swap the symmetry
+//* 1 2 3     3 2 1     3 6 9
+//* 4 5 6  => 6 5 4  => 2 5 8
+//* 7 8 9     9 8 7     1 4 7
+
+void anti_rotate(vector<vector<int> > &matrix) {
+    for (auto vi : matrix) reverse(vi.begin(), vi.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
+*/
